0d2cad446adb9b54b09c86ba15094202ac138a54
Valeriy <valeriy.elykomov@gmail.com>
2024-04-23 11:43:22 +0500

    Create extensions

---
+++ GigaChatSDK/Extensions/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace GigaChatSDK.Extensions
+{
+    internal static class ObjectExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static T ThrowIfNull<T>(
+            this T? value,
+            [CallerArgumentExpression(nameof(value))] string? parameterName = default) =>
+            value ?? throw new ArgumentNullException(parameterName);
+    }
+}
---
+++ GigaChatSDK/Extensions/RestResponseExtenisons.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Runtime.CompilerServices;
+
+namespace GigaChatSDK.Extensions
+{
+    internal static class RestResponseExtenisons
+    {
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //internal static TResponseType DeserializeContent<TResponseType>(
+        //this RestResponse response)
+        //{
+        //    if (response.Content is null)
+        //    {
+        //        throw new RequestException(
+        //            message: "Контент ответа ничего в себе не содержит (null)",
+        //            httpStatusCode: response.StatusCode);
+        //    }
+
+        //    TResponseType? deserializedObject;
+
+        //    try
+        //    {
+        //        deserializedObject = JsonConvert.DeserializeObject<TResponseType>(response.Content);
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        throw CreateRequestException(
+        //            response: response,
+        //            message: "Ошибка десериализации",
+        //            exception: exception);
+        //    }
+
+        //    if (deserializedObject is null)
+        //    {
+        //        throw CreateRequestException(
+        //            response: response,
+        //            message: "Десериализованный объект ничего в себе не содержит (null)");
+        //    }
+
+        //    return deserializedObject;
+        //}
+
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //static RequestException CreateRequestException(
+        //    RestResponse response,
+        //    string message,
+        //    Exception? exception = default) =>
+        //    exception is null
+        //        ? new(
+        //            message: message,
+        //            httpStatusCode: response.StatusCode
+        //        )
+        //        : new(
+        //            message: message,
+        //            httpStatusCode: response.StatusCode,
+        //            innerException: exception
+        //        );
+    }
+}
