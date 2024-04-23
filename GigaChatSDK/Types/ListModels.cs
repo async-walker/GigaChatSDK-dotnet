3a92f31d313d90bcdc29e4544d3481501ddebaed
Valeriy <valeriy.elykomov@gmail.com>
2024-04-23 11:44:18 +0500

    Create types for get model list method

---
+++ GigaChatSDK/Types/ListModels.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace GigaChatSDK.Types
+{
+    public sealed class ListModels
+    {
+        [JsonProperty("data")]
+        public List<ModelInfo> Models { get; set; } = default!;
+        [JsonProperty("object")]
+        public string Type { get; set; } = default!;
+    }
+}
---
+++ GigaChatSDK/Types/ModelInfo.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace GigaChatSDK.Types
+{
+    public sealed class ModelInfo
+    {
+        [JsonProperty("Id")]
+        public string Name { get; set; } = default!;
+        [JsonProperty("object")]
+        public string EntityType { get; set; } = default!;
+        [JsonProperty("owned_by")]
+        public string Owner { get; set; } = default!;
+    }
+}
