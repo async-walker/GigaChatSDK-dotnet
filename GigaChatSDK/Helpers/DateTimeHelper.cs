80049bac008c16abc62d72cec983c766ac2e3edc
Valeriy <valeriy.elykomov@gmail.com>
2024-04-23 11:43:31 +0500

    Update DateTimeHelper.cs

--- GigaChatSDK/Helpers/DateTimeHelper.cs
+++ GigaChatSDK/Helpers/DateTimeHelper.cs
@@ -4,10 +4,9 @@
     {
         public static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
         {
-            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dateTime = dateTime.AddMilliseconds(unixTimeStamp).ToLocalTime();
+            var dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(unixTimeStamp);
             
-            return dateTime;
+            return dateTimeOffset.DateTime;
         }
     }
 }
