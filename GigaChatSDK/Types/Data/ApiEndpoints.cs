d2deb272bb36b095a0a726f58967a63c462021d4
Valeriy <valeriy.elykomov@gmail.com>
2024-04-23 11:43:49 +0500

    Update ApiEndpoints.cs

--- GigaChatSDK/Types/Data/ApiEndpoints.cs
+++ GigaChatSDK/Types/Data/ApiEndpoints.cs
@@ -3,5 +3,6 @@
     internal static class ApiEndpoints
     {
         public const string AccessTokenEndpoint = "https://ngw.devices.sberbank.ru:9443/api/v2/";
+        public const string GigaChatEndpoint = "https://gigachat.devices.sberbank.ru/api/v1/";
     }
 }
