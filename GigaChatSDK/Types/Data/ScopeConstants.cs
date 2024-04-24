769af2f250601356b28a8543ecbdf591dcde457e
Valeriy <valeriy.elykomov@gmail.com>
2024-04-24 11:48:09 +0500

    Update ScopeConstants.cs

--- GigaChatSDK/Types/Data/ScopeConstants.cs
+++ GigaChatSDK/Types/Data/ScopeConstants.cs
@@ -1,17 +1,8 @@
 ﻿namespace GigaChatSDK.Types.Data
 {
-    /// <summary>
-    /// Строковые константы версий API
-    /// </summary>
-    public static class ScopeConstants
+    internal static class ScopeConstants
     {
-        /// <summary>
-        /// Версия API для физических лиц
-        /// </summary>
         public const string PERSONAL_SCOPE = "GIGACHAT_API_PERS";
-        /// <summary>
-        /// Версия API для ИП / юридических лиц
-        /// </summary>
         public const string CORPORATIVE_SCOPE = "GIGACHAT_API_CORP";
     }
 }
