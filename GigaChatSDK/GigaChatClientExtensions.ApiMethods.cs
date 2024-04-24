8c1ece3e04381856ebbebe70ea73ec55076b7a8b
Valeriy <valeriy.elykomov@gmail.com>
2024-04-24 11:47:58 +0500

    Create GigaChatClientExtensions.ApiMethods.cs

---
+++ GigaChatSDK/GigaChatClientExtensions.ApiMethods.cs
@@ -0,0 +1,18 @@
+﻿using GigaChatSDK.Extensions;
+using GigaChatSDK.Requests.AvailableMethods;
+using GigaChatSDK.Types;
+
+namespace GigaChatSDK
+{
+    public static class GigaChatClientExtensions
+    {
+        public static async Task<ListModels> GetListModelsAsync(
+            this IGigaChatClient client,
+            CancellationToken cancellationToken = default) =>
+            await client.ThrowIfNull()
+                .MakeRequestAsync(
+                    request: new GetListModelsRequest(),
+                    cancellationToken)
+                .ConfigureAwait(false);
+    }
+}
