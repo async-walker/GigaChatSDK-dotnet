e1a48f14037ffa252a444bd3ea48646f77055ee1
Valeriy <valeriy.elykomov@gmail.com>
2024-05-26 19:20:09 +0500

    Delete constants

--- GigaChatSDK/Types/Data/ScopeConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GigaChatSDK.Types.Data
-{
-    internal static class ScopeConstants
-    {
-        public const string PERSONAL_SCOPE = "GIGACHAT_API_PERS";
-        public const string CORPORATIVE_SCOPE = "GIGACHAT_API_CORP";
-    }
-}
