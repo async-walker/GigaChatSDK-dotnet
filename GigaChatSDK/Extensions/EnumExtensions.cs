ad0e10121d1c3241b92411d916bfb5a9d661d939
Valeriy <valeriy.elykomov@gmail.com>
2024-05-26 19:19:23 +0500

    Create EnumExtensions.cs

---
+++ GigaChatSDK/Extensions/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace GigaChatSDK.Extensions
+{
+    internal static class EnumExtensions
+    {
+        public static string GetEnumMemberValue<T>(this T value)
+            where T : struct, IConvertible
+        {
+            return typeof(T)
+                .GetTypeInfo()
+                .DeclaredMembers
+                .SingleOrDefault(x => x.Name == value.ToString())
+                ?.GetCustomAttribute<EnumMemberAttribute>(false)
+                ?.Value!;
+        }
+    }
+}
