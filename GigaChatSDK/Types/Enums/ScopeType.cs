521cfc31da1b44b0fe9620f770fcb160cf37e301
Valeriy <valeriy.elykomov@gmail.com>
2024-05-26 19:20:15 +0500

    Create ScopeType.cs

---
+++ GigaChatSDK/Types/Enums/ScopeType.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace GigaChatSDK.Types.Enums
+{
+    /// <summary>
+    /// Версия API для использования
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ScopeType
+    {
+        /// <summary>
+        /// Для физических лиц
+        /// </summary>
+        [EnumMember(Value = "GIGACHAT_API_PERS")]
+        Personal,
+        /// <summary>
+        /// Для ИП/ЮЛ
+        /// </summary>
+        [EnumMember(Value = "GIGACHAT_API_CORP")]
+        Corporative
+    }
+}
