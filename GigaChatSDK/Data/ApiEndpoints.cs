af49d93df3f9c04737ebf088988369a5ba5a76e8
Valeriy <valeriy.elykomov@gmail.com>
2024-07-06 19:20:28 +0500

    Move base url's to Options

--- GigaChatSDK/Data/ApiEndpoints.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GigaChatSDK.Data
-{
-    internal static class ApiEndpoints
-    {
-        public const string AccessTokenEndpoint = "https://ngw.devices.sberbank.ru:9443/api/v2";
-        public const string GigaChatEndpoint = "https://gigachat.devices.sberbank.ru/api/v1";
-    }
-}
--- GigaChatSDK/GigaChatClientOptions.cs
+++ GigaChatSDK/GigaChatClientOptions.cs
@@ -7,6 +7,8 @@ namespace GigaChatSDK
         public ScopeType Scope { get; }
         public string AuthData { get; }
         public bool AutoRefreshToken { get; }
+        public string BaseRefreshTokenUrl { get; }
+        public string BaseRequestUrl { get; }
 
         public GigaChatClientOptions(
             string authData,
@@ -16,6 +18,8 @@ namespace GigaChatSDK
             AuthData = authData;
             Scope = scope;
             AutoRefreshToken = autoRefreshToken;
+            BaseRefreshTokenUrl = "https://ngw.devices.sberbank.ru:9443/api/v2";
+            BaseRequestUrl = "https://gigachat.devices.sberbank.ru/api/v1";
         }
     }
 }
